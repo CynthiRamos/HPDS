3dc4547f310798b0739ce755b62f42e8c7715fa9
Cynthi <Cynthi@10.230.170.85>
2013-02-14 11:41:59 +0000

    14-02-13 Test and unaryOperators

---
+++ Documents/NetBeansProjects/Syntactic Tree/test/syntacticTreeUnaryOperators/IncreaseUnaryOperatorNodeTests.java
@@ -0,0 +1,19 @@
+
+package syntacticTreeUnaryOperators;
+
+import org.junit.Assert;
+import org.junit.Test;
+import syntactic.tree.ConstantNode;
+import syntactic.tree.UnaryOperationalNodes.IncreaseUnaryOperatorNode;
+
+public class IncreaseUnaryOperatorNodeTests {
+    @Test
+    public void IncreaseUnaryOperatorNodeTests_Zero() {
+        ConstantNode number = new ConstantNode(0);
+        IncreaseUnaryOperatorNode increase = 
+                new IncreaseUnaryOperatorNode(number);
+        Assert.assertEquals(1, increase, 0.0);
+       
+    }
+    
+  }
