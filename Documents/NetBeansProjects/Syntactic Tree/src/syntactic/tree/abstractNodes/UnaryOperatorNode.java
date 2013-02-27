6f9700f2a122a7a06864683ee00205767a0d1813
Cynthi <Cynthi@10.230.170.85>
2013-02-27 11:18:52 +0000

    all test done.

--- Documents/NetBeansProjects/Syntactic Tree/src/syntactic/tree/abstractNodes/UnaryOperatorNode.java
+++ Documents/NetBeansProjects/Syntactic Tree/src/syntactic/tree/abstractNodes/UnaryOperatorNode.java
@@ -1,16 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package syntactic.tree.abstractNodes;
 
 import syntactic.tree.Node;
-import syntactic.tree.abstractNodes.OperationalNode;
 
-/**
- *
- * @author Cynthi
- */
 public abstract class UnaryOperatorNode extends OperationalNode  {
     private Node center;
 
