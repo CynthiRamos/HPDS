224fe29d290d93a0428aec320b7d30686397af0d
Cynthi <Cynthi@10.230.170.85>
2013-03-25 11:37:40 +0000

    Creation of new abstract type- Integer&Double

---
+++ Documents/NetBeansProjects/Syntactic Tree/src/Types/DoubleType.java
@@ -0,0 +1,11 @@
+
+package Types;
+
+import syntactic.tree.Type;
+
+public class DoubleType extends Type <java.lang.Double>{
+
+    public DoubleType(Double value) {
+        super(value);
+    }
+}
---
+++ Documents/NetBeansProjects/Syntactic Tree/src/Types/IntegerType.java
@@ -0,0 +1,12 @@
+
+package Types;
+
+import syntactic.tree.Type;
+
+public class IntegerType extends Type <java.lang.Integer> {
+
+    public IntegerType(Integer value) {
+        super(value);
+    }   
+  
+}
