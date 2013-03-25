6558630331a841144cac82d3fa1eb3c80c4c64e1
Cynthi <Cynthi@10.230.170.85>
2013-03-25 11:02:17 +0000

    Creation of new abstract type

---
+++ Documents/NetBeansProjects/Syntactic Tree/src/syntactic/tree/Type.java
@@ -0,0 +1,13 @@
+
+package syntactic.tree;
+
+public abstract class Type<T> {
+    private T value;
+
+    public Type(T value){
+        this.value = value;
+    }
+    public T getValue() {
+        return value;
+    }  
+}
